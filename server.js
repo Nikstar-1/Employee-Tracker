2f307213d94c67d8e3244bfa474ed611c2da75b4
nikki <rk_787@hotmail.co.uk>
2020-12-10 00:48:25 +0000

    Inquirer Prompts added

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost",
   
-    // Your port; if not 3306
+  //Port 3306
     port: 3306,
     user: "start",
     password: "start",
     database: ""
   });
+
   //First log in prompts
   inquirer
   .prompt({
@@ -29,14 +30,75 @@ const connection = mysql.createConnection({
   })
   
 
-
+  //Department 
   inquirer.prompt({
       
     type: "input",
     message: "What is the name of the department?",
-    name: "deptName"
+    name: "dptName"
 
-})
+  }) 
+ 
+  //Add Role
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What's the name of the role?",
+        name: "roleName"
+      },
+      {
+        type: "input",
+        message: "What is the salary for this role?",
+        name: "totalSalary"
+      },
+      {
+        type: "input",
+        message: "What is the department id number?",
+        name: "dptID"
+      }
+    ])
+
+    //Add Employee
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What's the first name of the employee?",
+        name: "employeeFirstName"
+      },
+      {
+        type: "input",
+        message: "What's the last name of the employee?",
+        name: "employeeLastName"
+      },
+      {
+        type: "input",
+        message: "What is the employee's role id number?",
+        name: "roleIDNumber"
+      },
+      {
+        type: "input",
+        message: "What is the manager id number?",
+        name: "managerID"
+      }
+    ])
+   
+    //Update Employee
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Which employee would you like to update?",
+        name: "employeeUpdate"
+      },
+
+      {
+        type: "input",
+        message: "What do you want to update to?",
+        name: "updateRole"
+      }
+    ])
 
   .then((answer) => {
     let readmeContent = generateReadMeMarkdown(answer);
